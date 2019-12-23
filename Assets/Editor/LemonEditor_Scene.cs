71cce7a2dd500175265a12a9e90a2c64acb80f4c
onelei <ahleiwolong@163.com>
2019-12-23 10:39:24 +0800

    *modify file path

--- Assets/Editor/LemonEditor_Scene.cs
+++ Assets/Editor/LemonEditor_Scene.cs
@@ -2,34 +2,37 @@
 using UnityEditor.SceneManagement;
 using UnityEngine;
 
-public class LemonEditor_Scene : Editor
+namespace Lemon
 {
-    [MenuItem("Lemon/场景/技能编辑器 _F5")]
-    static private void OpenScene_Start()
+    public class LemonEditor_Scene : Editor
     {
-        OpenScene(Application.dataPath + "/Scenes/Start.unity");
-    }
-
-    [MenuItem("Lemon/场景/技能编辑器 _F6")]
-    static private void OpenScene_SkillSystemEditor()
-    {
-        OpenScene(Application.dataPath + "/Scenes/Editor/SkillSystemEditor.unity");
-    }
-
-    static private void OpenScene(string sceneName)
-    {
-        if (string.IsNullOrEmpty(sceneName))
+        [MenuItem("Lemon/场景/技能编辑器 _F5")]
+        static private void OpenScene_Start()
         {
-            QLog.LogErrorEditor("无法打开场景，场景名字为空！！！");
-            return;
+            OpenScene(Application.dataPath + "/Lemon/Scenes/Start.unity");
         }
 
-        if (!EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+        [MenuItem("Lemon/场景/技能编辑器 _F6")]
+        static private void OpenScene_SkillSystemEditor()
         {
-            QLog.LogErrorEditor("请保存当前场景");
-            return;
+            OpenScene(Application.dataPath + "/Lemon/Scenes/SkillSystemEditor.unity");
         }
 
-        EditorSceneManager.OpenScene(sceneName);
+        static private void OpenScene(string sceneName)
+        {
+            if (string.IsNullOrEmpty(sceneName))
+            {
+                QLog.LogErrorEditor("无法打开场景，场景名字为空！！！");
+                return;
+            }
+
+            if (!EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+            {
+                QLog.LogErrorEditor("请保存当前场景");
+                return;
+            }
+
+            EditorSceneManager.OpenScene(sceneName);
+        }
     }
 }
