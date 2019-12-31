b64b8e7b10099431be8288dd7df28e1b9e7d20a2
onelei <ahleiwolong@163.com>
2019-12-31 19:02:32 +0800

    *增加Button组件的封装

---
+++ Assets/Editor/Lemon/LemonUI/QButtonEditor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Lemon.UI
+{
+    [CustomEditor(typeof(QButton), true)]
+    public class QButtonEditor : UnityEditor.UI.ButtonEditor
+    {
+        [MenuItem("GameObject/UI/QButton", false, 1998)]
+        public static void AddComponent()
+        {
+            QButton component = UtilEditor.ExtensionComponentWhenCreate<QButton>(typeof(QButton).ToString());
+            //设置默认值
+            SetDefaultValue(component);
+        }
+
+        QButton component;
+        public override void OnInspectorGUI()
+        {
+            component = (QButton)target;
+            base.OnInspectorGUI();
+            if (!component.bInit)
+            {
+                component.bInit = true;
+                SetDefaultValue(component);
+            }
+        }
+
+        private static void SetDefaultValue(QButton component)
+        {
+            if (component == null)
+                return;
+        } 
+    }
+}
---
+++ Assets/Lemon/Scripts/LemonUI/QButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Lemon.UI
+{
+    [AddComponentMenu("UI/QButton")]
+    public class QButton : Button
+    {
+        [HideInInspector]
+        public bool bInit = false;
+    }
+}
